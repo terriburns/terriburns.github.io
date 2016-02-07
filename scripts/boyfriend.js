a6be04a640ead461e7bb3c9c9dd1a9d494cd76e6
Terri Burns <terri.burns@nyu.edu>
2016-02-07 12:35:04 -0500

    Update js functions

--- scripts/boyfriend.js
+++ scripts/boyfriend.js
@@ -1,10 +1,15 @@
 var response = "";
 while (response != 'Goodbye!'){
+  console.log("it got here");
   askQuestion();
 }
 
 //method to ask question, store input, and show the next questions
 askQuestion = function(){
+  //REMOVE FIRST QUESTION:
+  //
+  
+  //ask subsequent questions depending on name
   document.getElementById("name").submit();
   if (fname != "terri"){
     //do non-boyfriend stuff
