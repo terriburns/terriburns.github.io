cd2fb7ba0e4af3137dacb44b94c441af101dfb67
Terri Burns <terri.burns@nyu.edu>
2016-02-06 21:19:57 -0500

    Start adding boyfriend scripts lol

--- scripts/boyfriend.js
+++ scripts/boyfriend.js
@@ -1,3 +1,21 @@
-var readlineSync = require('readline-sync');
+var response = "";
+while (response != 'Goodbye!'){
+  askQuestion();
+}
 
-var greeting = readlinSync.question("Hi there. What's your name?");
+//method to ask question, store input, and show the next questions
+askQuestion = function(){
+  document.getElementById("name").submit();
+  if (fname != "terri"){
+    //do non-boyfriend stuff
+    nonBoyfriend(fname);
+  }
+  else {
+    //do boyfriend stuff
+    boyfriend(name);
+  }
+  nonBoyfriend = function(name){
+  }
+  boyfriend = function(name){
+  }
+}
