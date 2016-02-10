dfd87bf4547119f447f1430a0389addc8a6f6883
Terri Burns <terri.burns@nyu.edu>
2016-02-10 12:45:44 -0500

    Sigh

--- scripts/boyfriend.php
+++ scripts/boyfriend.php
@@ -3,16 +3,11 @@ if($_POST['formSubmit'] == "Submit")
 {
   $errorMessage = "";
 
-  if(empty($_POST['formMovie'])) 
-  {
-    $errorMessage .= "<li>You forgot to enter a movie!</li>";
-  }
   if(empty($_POST['formName'])) 
   {
     $errorMessage .= "<li>You forgot to enter a name!</li>";
   }
 
-  $varMovie = $_POST['formMovie'];
   $varName = $_POST['formName'];
 
   if(!empty($errorMessage)) 
